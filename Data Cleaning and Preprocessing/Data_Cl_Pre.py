0a8991aca781100f57d70d55983c10df51ff03c3
sindhura-nk <nk.sindhura@gmail.com>
2026-02-26 11:09:15 +0530

    Data cleaning preprocessing package updated

---
+++ Data Cleaning and Preprocessing/Data_Cl_Pre.py
@@ -0,0 +1,47 @@
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+def data_prepration_cat_con(X,cat,con):
+
+    cat = list(X.select_dtypes(include='str').columns)
+    con = list(X.select_dtypes(include='number').columns)
+
+    num_pipe = make_pipeline(
+        SimpleImputer(strategy='median'),
+        StandardScaler()
+    )
+
+    cat_pipe = make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        OneHotEncoder(handle_unknown='ignore',sparse_output=False)
+    )
+
+    pre = ColumnTransformer([
+        ('cat',cat_pipe,cat),
+        ('con',num_pipe,con)
+    ]).set_output(transform='pandas')
+
+    X_pre = pre.fit_transform(X)
+    return X_pre
+
+def data_prepration_cat(X):
+
+    cat_pipe = make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        OneHotEncoder(handle_unknown='ignore',sparse_output=False)
+    ).set_output(transform='pandas')
+
+    X_pre = cat_pipe.fit_transform(X)
+    return X_pre
+
+def data_prepration_con(X):
+
+    num_pipe = make_pipeline(
+        SimpleImputer(strategy='median'),
+        StandardScaler()
+    ).set_output(transform='pandas')
+
+    X_pre = num_pipe.fit_transform(X)
+    return X_pre

1ff639e900ebbdde8c7df3fd6bca670f57d3a630
sindhura-nk <nk.sindhura@gmail.com>
2026-03-02 10:54:59 +0530

    datacleaning file changed

--- Data_Cleaning_and_Preprocessing/Data_Cl_Pre.py
+++ Data_Cleaning_and_Preprocessing/Data_Cl_Pre.py
@@ -5,7 +5,7 @@ from sklearn.compose import ColumnTransformer
 
 def data_prepration_cat_con(X):
 
-    cat = list(X.select_dtypes(include='category').columns) # include='str'
+    cat = list(X.select_dtypes(include='str').columns) # include='str'
     con = list(X.select_dtypes(include='number').columns)
 
     num_pipe = make_pipeline(
